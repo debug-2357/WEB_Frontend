ab0c8aec3a726409baeba46bcf95e5baa1ba91a8
EunJungKim <dmswjd7281@naver.com>
2022-07-27 18:53:13 +0900

    [#2]modify global setting

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import style from '../public/css/applayout.module.css';
 
 const AppLayout = ({ children }) => {
   return (
     <>
-      <div className={style.HeaderBody}>헤더영역</div>
+      <div className={'HeaderBody'}>헤더영역</div>
       {children}
-      <div className={style.FooterBody}>copyrightⓒ2022. debug. All rights reserved.</div>
+      <div className={'FooterBody'}>copyrightⓒ2022. debug. All rights reserved.</div>
     </>
   );
 };
