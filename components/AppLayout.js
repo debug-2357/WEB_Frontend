5f0603d1480f69a6400aab181233fbf973e91c6e
EunJungKim <dmswjd7281@naver.com>
2022-07-27 23:50:27 +0900

    [#2]modify settings2

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Button } from '@mui/material';
 
 const AppLayout = ({ children }) => {
   return (
     <>
-      <div className={'HeaderBody'}>헤더영역</div>
+      <div className="HeaderBody">
+        <div>
+          <img src="/images/Logo.png" className="LogoImg" />
+        </div>
+        <div>
+          <Button variant="text" size="small" className="MainBtn">
+            Main
+          </Button>
+          <Button variant="text" size="small" className="RecruitBtn">
+            Recruit
+          </Button>
+        </div>
+      </div>
       {children}
       <div className={'FooterBody'}>copyrightⓒ2022. debug. All rights reserved.</div>
     </>
