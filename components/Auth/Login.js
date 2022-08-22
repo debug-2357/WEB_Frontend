839bdafb2623cb7cac02553026ad544c4e9df032
dladncks1217 <dlaxodud1217@gmail.com>
2022-08-22 20:21:10 +0900

    [#16] Add Auth actions, Add Auth reducers, Link Store to Auth Components, Modify Auth Components Codes

--- components/Auth/Login.js
+++ components/Auth/Login.js
@@ -1,87 +1,84 @@
 import { Box, Button } from '@mui/material';
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import styles from '../../public/css/login.module.css';
 import PasswordIcon from '@mui/icons-material/Password';
 import InputAdornment from '@mui/material/InputAdornment';
 import IconButton from '@mui/material/IconButton';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import { TextField } from '@mui/material';
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import { useDispatch } from 'react-redux';
+import { loginAction } from '../../actions/auth';
 
 const Login = () => {
-  const [values, setValues] = useState({
-    password: '',
-    showPassword: false,
+  const dispatch = useDispatch();
+
+  const [userId, setUserId] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onChangeUserId = useCallback((e) => {
+    setUserId(e.target.value);
   });
-  const handleChange = (prop) => (event) => {
-    setValues({ ...values, [prop]: event.target.value });
-  };
 
-  const handleClickShowPassword = () => {
-    setValues({
-      ...values,
-      showPassword: !values.showPassword,
-    });
-  };
+  const onChangePassword = useCallback((e) => {
+    setPassword(e.target.value);
+  });
 
-  const handleMouseDownPassword = (event) => {
-    event.preventDefault();
-  };
+  const onSubmit = useCallback((e) => {
+    const data = { userId, password };
+    dispatch(loginAction(data));
+  });
+
+  const handleRedirectPage = useCallback((e) => {
+    e.preventDefault();
+    location.href = '/auth/join';
+  });
+
   return (
-    <div className={`${styles.LoginWrap}`}>
-      <Box component="div" sx={{ p: 3, width: 500, height: 500 }} className={`${styles.LoginBoxWrap}`}>
+    <div className={styles.LoginWrap}>
+      <Box component="div" className={styles.LoginBoxWrap}>
         <h2>로그인</h2>
         <hr />
-        <div className={`${styles.TextFieldWrap}`}>
-          <div className={`${styles.TextField}`}>
-            <TextField
-              required
-              id="input-with-icon-textfield"
-              label="ID"
-              sx={{ width: '15em' }}
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <AccountCircle />
-                  </InputAdornment>
-                ),
-              }}
-              variant="standard"
-            />
-          </div>
-          <div className={`${styles.TextField}`}>
-            <TextField
-              required
-              id="input-with-icon-textfield"
-              label="PASSWORD"
-              sx={{ width: '15em' }}
-              type={values.showPassword ? 'text' : 'password'}
-              value={values.password}
-              onChange={handleChange('password')}
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <PasswordIcon />
-                  </InputAdornment>
-                ),
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton aria-label="toggle password visibility" onClick={handleClickShowPassword} onMouseDown={handleMouseDownPassword}>
-                      {values.showPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-              variant="standard"
-            />
-          </div>
+        <div className={styles.TextFieldWrap}>
+          <TextField
+            required
+            id="input-with-icon-textfield"
+            label="ID"
+            className={styles.TextField}
+            value={userId}
+            onChange={onChangeUserId}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <AccountCircle />
+                </InputAdornment>
+              ),
+            }}
+            variant="standard"
+          />
+
+          <TextField
+            required
+            id="input-with-icon-textfield"
+            label="PASSWORD"
+            className={styles.TextField}
+            type="password"
+            value={password}
+            onChange={onChangePassword}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <PasswordIcon />
+                </InputAdornment>
+              ),
+            }}
+            variant="standard"
+          />
         </div>
-        <div className={`${styles.ButtonGroup}`}>
-          <Button variant="contained" sx={{ mr: 5, width: '10em' }}>
+        <div className={styles.ButtonGroup}>
+          <Button variant="contained" onClick={onSubmit}>
             로그인
           </Button>
-          <Button variant="contained" sx={{ width: '10em' }}>
+          <Button variant="contained" onClick={handleRedirectPage}>
             회원가입
           </Button>
         </div>
